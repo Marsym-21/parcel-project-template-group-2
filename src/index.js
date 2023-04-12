5a5d1d3736f639f2f6bd3b1aac821b9cbcb80be5
Marsym-21 <mini.garnett.21@gmail.com>
2023-04-12 18:38:12 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-fetch('https://books-backend.p.goit.global/books/category-list')
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  });
+// fetch('https://books-backend.p.goit.global/books/category-list')
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
+//     console.log(data);
+//   });

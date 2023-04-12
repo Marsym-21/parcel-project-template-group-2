92fd94258b2ac8d9e6ae57c678746f906a7c62ed
Marsym-21 <114507956+Marsym-21@users.noreply.github.com>
2023-04-12 16:39:18 +0000

    Deploying to gh-pages from @ Marsym-21/parcel-project-template-group-2@e25b67e9d096e0c7e02b639ade985d849f8d025f 🚀

---
+++ index.560173d1.js
@@ -0,0 +1,2 @@
+fetch("https://books-backend.p.goit.global/books/category-list").then((function(o){return o.json()})).then((function(o){console.log(o)}));
+//# sourceMappingURL=index.560173d1.js.map

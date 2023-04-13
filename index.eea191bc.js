d0c8a263f6a2746bc83cecc9fdfbb9f46a30d4ef
Marsym-21 <114507956+Marsym-21@users.noreply.github.com>
2023-04-13 07:29:34 +0000

    Deploying to gh-pages from @ Marsym-21/parcel-project-template-group-2@b21c6065dbbcadf8c2c3e7581695c0670165bf13 🚀

---
+++ index.eea191bc.js
@@ -0,0 +1,2 @@
+!function(){var s;s="sdfsd sdfsd dsfsd dsfsdf".split(""),console.log(s)}();
+//# sourceMappingURL=index.eea191bc.js.map
---
+++ index.f318cf3e.js
@@ -0,0 +1,2 @@
+!function(s){const d=s.split("");console.log(d)}("sdfsd sdfsd dsfsd dsfsdf");
+//# sourceMappingURL=index.f318cf3e.js.map

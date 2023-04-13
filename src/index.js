34ffd26b9c3dc81e7978d2ffe74cbc897a5dabf0
Marsym-21 <mini.garnett.21@gmail.com>
2023-04-13 12:56:20 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 //Приклад використання промісів з getBooksData.js
 
 import { getBookData } from './js/getBooksData.js';
+import { createCategoryString } from './js/getCategoryString.js';
 
-const booksInform = new getBookData((id = '643282b1e85766588626a085'));
+const booksInform = new getBookData(
+  (id = '643282b1e85766588626a085'),
+  (category = `${createCategoryString('Combined Print and E-Book Fiction')}`)
+);
 
 function renderCategoryList() {
   const body = document.querySelector('body');
   const list = document.createElement('ul');
   body.append(list);
-  let categoryArray = [];
+  // let categoryArray = [];
   booksInform
-    .getPromCategoryList()
+    .getPromCategory()
     .then(books => {
-      categoryArray = books.map(book => `<li>${book.list_name}</li>`).join('');
+      // categoryArray = books.map(book => `<li>${book.list_name}</li>`).join('');
       console.log(books);
-      list.insertAdjacentHTML('beforeend', categoryArray);
+      // list.insertAdjacentHTML('beforeend', categoryArray);
     })
     .catch(error => {
       console.log(error);
--- src/js/getCategoryString.js
+++ src/js/getCategoryString.js
@@ -1,6 +1,14 @@
 export function createCategoryString(string) {
   const array = string.split('');
-  console.log(array);
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    let letter = array[i];
+    if (letter === ' ') {
+      letter = '%20';
+    }
+    newArray.push(letter);
+  }
+  return newArray.join('');
 }
 
-createCategoryString('sdfsd sdfsd dsfsd dsfsdf');
+createCategoryString('Combined Print and E-Book Fiction');

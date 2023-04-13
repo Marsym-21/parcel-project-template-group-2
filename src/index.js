b325d4cfe99dd5b8df527520ae36d8e04d245ffc
Marsym-21 <mini.garnett.21@gmail.com>
2023-04-13 20:22:02 +0200

    123

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import './js/toggle-theme';
 import { getBookData } from './js/getBooksData.js';
 import { createCategoryString } from './js/getCategoryString.js';
 
+// Змінна яка підтверджує додавання книги до ShoppingList
+export let isAddToShoppingList = true;
+// по дефолту повинна бути біла тема.
+// розрахунок адаптивного розміру шрифта = розмір шрифта * 100 / на максимальний розмір вьюпорта vw
+// скрол робити в css, складні скроли можливо в бібліотеці.
+// All Categories не ховати, hover/focus - toUpperCaseтекст.
+
 const booksInform = new getBookData(
   (id = '643282b1e85766588626a085'),
   (category = `${createCategoryString('Combined Print and E-Book Fiction')}`)

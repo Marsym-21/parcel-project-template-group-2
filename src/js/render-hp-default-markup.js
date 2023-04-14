b87e64bee4d3b9ed265613a08f1fb18a00330b37
Pavlo-V-M <letter.screen.1@gmail.com>
2023-04-14 22:45:56 +0300

    test-2
    get API
    render markup home page default
    add some styles

--- src/js/render-hp-default-markup.js
+++ src/js/render-hp-default-markup.js
@@ -68,30 +68,35 @@ function renderCategoryList() {
     .getPromTopBooks()
     .then(books => {
       const mainTitleName = document.createElement('h1');
+      mainTitleName.classList.add("default__main-title");
+      console.log(mainTitleName);
       sectionCategory.prepend(mainTitleName)
       mainTitleName.textContent = mainTitle;
      categoryArray = books.slice(0, 4).map(book => {
        const booksArray = book.books.map(data =>
          `<li class="books-list__item" data-id="${data._id}">
             <div class="item-wrap">
-            <div class = "item-img__wrap">
-             <img class="item-img"
+             <div class = "item-img__wrap">
+               <img class="item-img"
                 src="${data.book_image}" 
                 alt="${imgAttributeAlt}" 
-                width ="330px" 
-                height ="485px" />
-             </div>
-                <div class="item-title__wrap">
-                  <h3 class="item__name">${data.title}</h3>
-                  <p class="item__author">${data.author}</p>
-                </div>
+                width ="${data.book_image_width}" 
+                height ="${data.book_image_height}"
+               />
+              </div>
+              <div class="item-title__wrap">
+                <h3 class="item__name">${data.title.slice(0, 30)}${data.title.length > 30 ? '...' : ''}</h3>
+                <p class="item__author">${data.author}</p>
+              </div>
             </div>
           </li>`
        ).join('');
        return `<li class="categories-list__item">
-                 <h2 class="category">${book.list_name}</h2>
-                 <ul class="books-list">${booksArray}</ul>
-                 <button type="button">see more</button>
+                 <div class="categories-list__wrap">
+                   <h2 class="category">${book.list_name}</h2>
+                   <ul class="books-list">${booksArray}</ul>
+                   <button class="btn-see-more" type="button">see more</button>
+                 </div>
                 </li>`;
       });
       

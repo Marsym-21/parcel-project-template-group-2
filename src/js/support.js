7318545fc7a428825828061116f0a1a1c4912bae
NikitaSimakov <mishaaniskin@gmail.com>
2023-04-14 13:13:55 +0300

    breakpoints

--- src/js/support.js
+++ src/js/support.js
@@ -1,51 +1,60 @@
 import Swiper from 'swiper';
 import '../../node_modules/swiper/swiper.scss';
+import save from '../images/support/save.png';
+import hope from '../images/support/hope.png';
+import united from '../images/support/united.png';
+import medical from '../images/support/medical.png';
+import sans from '../images/support/sans.png';
+import razom from '../images/support/razom.png';
+import hunger from '../images/support/hunger.png';
+import vision from '../images/support/vision.png';
+import prytula from '../images/support/prytula.png';
 
 const supportItemsArr = [
   {
     title: 'Save the Children',
     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: null,
+    img: save,
   },
   {
     title: 'Project HOPE',
     url: 'https://www.projecthope.org/country/ukraine/',
-    img: null,
+    img: hope,
   },
   {
     title: 'UNITED24',
     url: 'https://u24.gov.ua/uk',
-    img: null,
+    img: united,
   },
   {
     title: 'International Medical Corps',
     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: null,
+    img: medical,
   },
   {
     title: 'Medicins Sans Frontieres',
     url: 'https://www.msf.org/ukraine',
-    img: null,
+    img: sans,
   },
   {
     title: 'RAZOM',
     url: 'https://www.razomforukraine.org/',
-    img: null,
+    img: razom,
   },
   {
     title: 'Action against hunger',
     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: null,
+    img: hunger,
   },
   {
     title: 'World vision',
     url: 'https://www.wvi.org/emergencies/ukraine',
-    img: null,
+    img: vision,
   },
   {
     title: 'Serhiy Prytula Charity Foundation',
     url: 'https://prytulafoundation.org/en',
-    img: null,
+    img: prytula,
   },
 ];
 
@@ -54,9 +63,10 @@ const supportItemsArr = [
 function addSupportItemsToList(arr) {
   return arr.map(
     item =>
-      `<li class="support_item swiper-slide"><a target="_blank" href="${
+      `<li class="support_item swiper-slide"><a class="support-link" target="_blank" href="${
         item.url
-      }">0${arr.indexOf(item) + 1} ${item.title}</a></li>`
+      }"><span class="support-number">0${arr.indexOf(item) + 1}</span>
+    <img class="support-logo" alt="${item.title}" src="${item.img}"></a></li>`
   );
 }
 
@@ -74,16 +84,22 @@ renderSupportList();
 const swiper = new Swiper('.swiper', {
   direction: 'vertical',
   slidesPerView: 4,
-  spaceBetween: 20,
-  loop: true,
+  // spaceBetween: 10,
+  // loop: true,
   // navigation: true,
-  slidesPerGroup: 4,
-  oneWayMovement: true,
+  // slidesPerGroup: 4,
+  // oneWayMovement: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 6,
+      slidesPerGroup: 6,
+      // spaceBetween: 20,
+      loop: true,
+    },
+  },
 });
 
 console.log(swiper);
-
-// swiper.slideNext();
 
 const supportButtonNext = document.querySelector('.support-button-next');
 supportButtonNext.addEventListener('click', () => {

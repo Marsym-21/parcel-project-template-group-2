b7990d8385307821527fdadfc6f484a304e62b82
Marsym-21 <mini.garnett.21@gmail.com>
2023-04-15 14:29:12 +0200

    123

--- src/js/toggle-theme.js
+++ src/js/toggle-theme.js
@@ -13,25 +13,20 @@ if (localStorage.getItem('theme') === 'dark') {
 
 checkbox.addEventListener('change', () => {
   if (checkbox.checked) {
-    console.log(getArrayAddList(object));
-    ref.body.classList.add('dark');
-    ref.header.classList.add('dark');
     getArrayAddList(object);
     localStorage.setItem('theme', 'dark');
   } else {
-    ref.body.classList.remove('dark');
-    ref.header.classList.remove('dark');
     getArrayRemoveList(object);
     localStorage.setItem('theme', 'light');
   }
 });
 
 function getArrayAddList(array) {
-  const newArray = array.map(data => `ref.${data}.classList.add('dark');`);
-  return newArray;
+  array.forEach(data => ref[data].classList.add('dark'));
+  return array;
 }
 
 function getArrayRemoveList(array) {
-  const newArray = array.map(data => `ref.${data}.classList.remove('dark');`);
-  return newArray;
+  array.forEach(data => ref[data].classList.remove('dark'));
+  return array;
 }

a3eeabf7e17197cc55a394c31497dcc7b721cc51
Marsym-21 <mini.garnett.21@gmail.com>
2023-04-15 10:37:44 +0200

    123

--- src/js/toggle-theme.js
+++ src/js/toggle-theme.js
@@ -1,19 +1,39 @@
 const checkbox = document.getElementById('theme-checkbox');
-const body = document.querySelector('body');
+
+const ref = {
+  body: document.querySelector('body'),
+  header: document.querySelector('.header_btn__shopping__light'),
+};
+
+const object = Object.keys(ref);
+console.log(object);
+
+// const white = document.querySelectorAll('.white');
+
+// body.classList.add('white');
 
 // Check local storage for theme preference
 if (localStorage.getItem('theme') === 'dark') {
   checkbox.checked = true;
-  body.classList.add('dark');
+  // white.classList.add('dark');
 }
 
 // Toggle theme on checkbox change
 checkbox.addEventListener('change', () => {
   if (checkbox.checked) {
-    body.classList.add('dark');
+    getArray(object);
+    // ref.body.classList.add('dark');
+    // ref.header.classList.add('dark');
     localStorage.setItem('theme', 'dark');
   } else {
-    body.classList.remove('dark');
+    getArrayRemoveList(object);
     localStorage.setItem('theme', 'light');
   }
 });
+
+function getArrayAddList(array) {
+  return array.map(data => data.classList.add('dark'));
+}
+function getArrayRemoveList(array) {
+  return array.map(data => data.classList.remove('dark'));
+}

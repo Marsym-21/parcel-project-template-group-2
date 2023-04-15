70de04906a9abd5653f6db6c2c18354c323a5cd0
Marsym-21 <mini.garnett.21@gmail.com>
2023-04-15 10:39:21 +0200

    123

--- src/js/toggle-theme.js
+++ src/js/toggle-theme.js
@@ -8,22 +8,13 @@ const ref = {
 const object = Object.keys(ref);
 console.log(object);
 
-// const white = document.querySelectorAll('.white');
-
-// body.classList.add('white');
-
-// Check local storage for theme preference
 if (localStorage.getItem('theme') === 'dark') {
   checkbox.checked = true;
-  // white.classList.add('dark');
 }
 
-// Toggle theme on checkbox change
 checkbox.addEventListener('change', () => {
   if (checkbox.checked) {
     getArray(object);
-    // ref.body.classList.add('dark');
-    // ref.header.classList.add('dark');
     localStorage.setItem('theme', 'dark');
   } else {
     getArrayRemoveList(object);

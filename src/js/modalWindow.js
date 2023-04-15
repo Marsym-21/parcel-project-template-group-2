b798b7f4d5821d8f42dc7fca8c703b966f071c9b
Yaroslava2022 <114984565+Yaroslava2022@users.noreply.github.com>
2023-04-15 14:28:12 +0300

    window

--- src/js/modalWindow.js
+++ src/js/modalWindow.js
@@ -1,30 +1,35 @@
 import { getBookData } from './getBooksData.js';
-const booksInform = new getBookData((id = '643282b2e85766588626a112'));
+
+const booksInform = new getBookData((id = '643282b1e85766588626a0b4'));
 const logoPath = new URL('../images/icons.svg', import.meta.url);
 let addedToShoppingBag = false;
-let booksArray = [];
-
-function renderBookInformation() {
-  const modalContainer = document.querySelector('.modal-container');
-
+const booksArray = [];
+const modalContainer = document.querySelector('.modal-container');
+ function renderBookInformation() {
+ 
   booksInform
     .getPromId()
     .then(data => {
-      function links(sms) {
-        const link = data.buy_links;
-        for (let i = 0; i < link.length; i += 1) {
-          if (link[i].name === `${sms}`) {
-            return link[i].url;
-          } else if (link[i].name !== `${sms}`) {
-            continue;
-          }
-        }
+
+      function links(sms) { 
+        const link = data.buy_links; 
+        for (let i = 0; i < link.length; i += 1) { 
+            if (link[i].name === `${sms}`) { 
+             return link[i].url; 
+            }else if (link[i].name !== `${sms}`) { 
+              continue; 
+          }} 
+      };
+       
+      function checkValue(value) {
+        if (value !== '') {
+        return value;
+          
+        } else {let message = 'No information'
+        return message;
       }
-
-      console.log(links('Amazon'));
-      console.log(links('Bookshop'));
-      console.log(links('Apple Books'));
-
+      }
+ 
       categoryArray = `
        <div class="modal-close__btn-wrapper">
       <button type='button' class='modal-close-btn js-modal-close-btn '>
@@ -76,7 +81,6 @@ function renderBookInformation() {
        </svg>
    </div>
 
-
        <div class ="modal-book__bookshop">
        <a class="bookshop__link link"
        href="${links('Bookshop')}"  
@@ -92,17 +96,64 @@ function renderBookInformation() {
        </div>
        </div>
        <div  class="modal-book__add__wrapper">  
-       <button type="button" class="modal-book__button btn__add" data-id="${
-         data.id
-       }">Add to shopping list</button>  
-         
+       <button type="button" class="modal-book__button" id="${data.id}">Add to shopping list</button>  
    </div>  
         `;
-
+ 
       modalContainer.innerHTML = categoryArray;
+      // modalContainer.classList.remove('is-hidden');
+      // modalContainer.classList.add('modal-open');
+
+      const closeButton = document.querySelector('.modal-close-btn');
+      closeButton.addEventListener('click', closeBtn);
+      function closeBtn() {
+      modalContainer.classList.add('is-hidden');
+      modalContainer.classList.remove('modal-open');
+          }
+
+      const addButton= document.querySelector('.modal-book__button');
+      addButton.addEventListener('click', () => {
+       
+        
+         if (booksArray.includes(id))  {
+          let bookIndex = booksArray.indexOf('${data.id}')
+      booksArray.splice(bookIndex, 1)
+      stringKey = JSON.stringify(booksArray); 
+ 
+      
+      addButton.textContent = "Add to shopping list"; 
+      addedToShoppingBag = false;
+      
+      addButton.classList.remove('btn__add');
+      console.log(booksArray);
+      localStorage.setItem('id', stringKey)
+      return;
+     
+      }  
+        else  
+        {
+          booksArray.push(id);
+          addButton.textContent = "Remove from the shopping list"
+          stringKey = JSON.stringify(booksArray); 
+          localStorage.setItem('id', stringKey);
+          addedToShoppingBag = true;
+         
+        console.log(booksArray);
+        addButton.classList.add('btn__add');}  
+        
+      
+      
+          })
     })
     .catch(error => {
       console.log(error);
     });
 }
-renderBookInformation();
+// renderBookInformation()
+
+const ourGallary = document.querySelector('.header-button')
+ourGallary.addEventListener('click', () => {
+  modalContainer.classList.remove('is-hidden');
+  renderBookInformation();
+ 
+ })

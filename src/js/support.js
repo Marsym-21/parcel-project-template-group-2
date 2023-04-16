e2046a22cd6d2e968802e5a814fe1ebd035c4e96
NikitaSimakov <mishaaniskin@gmail.com>
2023-04-16 15:16:33 +0300

    bug fix

--- src/js/support.js
+++ src/js/support.js
@@ -1,67 +1,6 @@
-// import supportItemsArr from './supportData';
+import { supportData } from './supportData';
 import Swiper from 'swiper';
 import '../../node_modules/swiper/swiper.scss';
-
-// console.log(supportItemsArr);
-import save from '../images/support/save.png';
-import hope from '../images/support/hope.png';
-import united from '../images/support/united.png';
-import medical from '../images/support/medical.png';
-import sans from '../images/support/sans.png';
-import razom from '../images/support/razom.png';
-import hunger from '../images/support/hunger.png';
-import vision from '../images/support/vision.png';
-import prytula from '../images/support/prytula.png';
-
-const supportItemsArr = [
-  {
-    title: 'Save the Children',
-    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: save,
-  },
-  {
-    title: 'Project HOPE',
-    url: 'https://www.projecthope.org/country/ukraine/',
-    img: hope,
-  },
-  {
-    title: 'UNITED24',
-    url: 'https://u24.gov.ua/uk',
-    img: united,
-  },
-  {
-    title: 'International Medical Corps',
-    url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: medical,
-  },
-  {
-    title: 'Medicins Sans Frontieres',
-    url: 'https://www.msf.org/ukraine',
-    img: sans,
-  },
-  {
-    title: 'RAZOM',
-    url: 'https://www.razomforukraine.org/',
-    img: razom,
-  },
-  {
-    title: 'Action against hunger',
-    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: hunger,
-  },
-  {
-    title: 'World vision',
-    url: 'https://www.wvi.org/emergencies/ukraine',
-    img: vision,
-  },
-  {
-    title: 'Serhiy Prytula Charity Foundation',
-    url: 'https://prytulafoundation.org/en',
-    img: prytula,
-  },
-];
-
-// console.log(document.querySelector('.support-list'));
 
 function addSupportItemsToList(arr) {
   return arr.map(
@@ -79,7 +18,7 @@ function renderSupportList() {
   const elSupportList = document.querySelector('.support-list');
   elSupportList.insertAdjacentHTML(
     'beforeend',
-    addSupportItemsToList(supportItemsArr).join(' ')
+    addSupportItemsToList(supportData).join(' ')
   );
 }
 renderSupportList();
@@ -104,7 +43,7 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-console.log(swiper);
+// console.log(swiper);
 
 const supportButtonNext = document.querySelector('.support-button-next');
 supportButtonNext.addEventListener('click', () => {

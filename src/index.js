37946cd59f2e730fe34f8d601553d841656cfd00
Marsym-21 <mini.garnett.21@gmail.com>
2023-04-16 16:01:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-// import './js/header-theme.js';
-// import './js/render-hp-default-markup.js';
+import './js/header-theme.js';
+import './js/render-hp-default-markup.js';
 import './js/support';
 import './js/toggle-theme';
 import './js/categories';
-// import './js/toggle-theme';
-// import './js/mobile-menu.js';
-// import './js/modalWindow.js';
+import './js/mobile-menu.js';
+import './js/modalWindow.js';
 
 //Приклад використання промісів з getBooksData.js
 // import { getBookData } from './js/getBooksData.js';

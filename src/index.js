361bea8eb40fff186bf259b03285368ea9ab6cf2
Marsym-21 <mini.garnett.21@gmail.com>
2023-04-17 17:44:30 +0200

    123

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './js/header-theme.js';
 import './js/render-hp-default-markup.js';
-import './js/support.js';
+// import './js/support.js';
 import './js/toggle-theme.js';
 import './js/categories.js';
 import './js/mobile-menu.js';

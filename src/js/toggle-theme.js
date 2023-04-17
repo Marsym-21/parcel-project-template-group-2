2083a4bc20a1e265ce8f102372b550391911c22c
Marsym-21 <mini.garnett.21@gmail.com>
2023-04-17 15:56:16 +0200

    123

--- src/js/toggle-theme.js
+++ src/js/toggle-theme.js
@@ -1,36 +1,36 @@
-const checkbox = document.getElementById('theme-checkbox');
-if (localStorage.getItem('theme') === 'dark') {
-  checkbox.checked = true;
-}
+// const checkbox = document.getElementById('theme-checkbox');
+// if (localStorage.getItem('theme') === 'dark') {
+//   checkbox.checked = true;
+// }
 
-let ref = {};
+// let ref = {};
 
-export function getObject() {
-  ref = {
-    body: document.querySelector('body'),
-    header: document.querySelector('.header_btn__shopping'),
-    newElem: document.querySelector('#seemore__btn'),
-  };
-  console.log(ref);
+// export function getObject() {
+//   ref = {
+//     body: document.querySelector('body'),
+//     header: document.querySelector('.header_btn__shopping'),
+//     newElem: document.querySelector('#seemore__btn'),
+//   };
+//   console.log(ref);
 
-  const object = Object.keys(ref);
+//   const object = Object.keys(ref);
 
-  checkbox.addEventListener('change', () => {
-    if (checkbox.checked) {
-      getArrayAddList(object);
-      localStorage.setItem('theme', 'dark');
-    } else {
-      getArrayRemoveList(object);
-      localStorage.setItem('theme', 'light');
-    }
-  });
-}
-function getArrayAddList(array) {
-  array.forEach(data => ref[data].classList.add('dark'));
-  return array;
-}
+//   checkbox.addEventListener('change', () => {
+//     if (checkbox.checked) {
+//       getArrayAddList(object);
+//       localStorage.setItem('theme', 'dark');
+//     } else {
+//       getArrayRemoveList(object);
+//       localStorage.setItem('theme', 'light');
+//     }
+//   });
+// }
+// function getArrayAddList(array) {
+//   array.forEach(data => ref[data].classList.add('dark'));
+//   return array;
+// }
 
-function getArrayRemoveList(array) {
-  array.forEach(data => ref[data].classList.remove('dark'));
-  return array;
-}
+// function getArrayRemoveList(array) {
+//   array.forEach(data => ref[data].classList.remove('dark'));
+//   return array;
+// }

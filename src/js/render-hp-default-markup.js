621e032beeda32ede344c455fb916d2f3ac96e71
Marsym-21 <mini.garnett.21@gmail.com>
2023-04-17 12:24:32 +0200

    123

--- src/js/render-hp-default-markup.js
+++ src/js/render-hp-default-markup.js
@@ -102,10 +102,10 @@ function renderCategoryList() {
       categoryHtml = categoryArray.join(''); // Join the array of HTML strings into a single string
       categoryList.insertAdjacentHTML('beforeend', categoryHtml); // Insert the HTML code into the ul element
 
-      Object = {
-        newElem: document.querySelector('#seemore__btn'),
-      };
-      getObject(Object);
+      // Object = {
+      //   newElem: document.querySelector('#seemore__btn'),
+      // };
+      getObject();
     })
     .catch(error => {
       console.log(error);
--- src/js/toggle-theme.js
+++ src/js/toggle-theme.js
@@ -1,37 +1,35 @@
 const checkbox = document.getElementById('theme-checkbox');
-
-const ref = {
-  body: document.querySelector('body'),
-  header: document.querySelector('.header_btn__shopping'),
-};
-
-let newObject;
-export function getObject(obj) {
-  newObject = { ...ref, ...obj };
-  console.log(newObject);
-}
-console.log(newObject);
-
 if (localStorage.getItem('theme') === 'dark') {
   checkbox.checked = true;
 }
 
-// checkbox.addEventListener('change', () => {
-//   if (checkbox.checked) {
-//     getArrayAddList(object);
-//     localStorage.setItem('theme', 'dark');
-//   } else {
-//     getArrayRemoveList(object);
-//     localStorage.setItem('theme', 'light');
-//   }
-// });
+export function getObject() {
+  const ref = {
+    body: document.querySelector('body'),
+    header: document.querySelector('.header_btn__shopping'),
+    newElem: document.querySelector('#seemore__btn'),
+  };
+  console.log(ref);
 
-// function getArrayAddList(array) {
-//   array.forEach(data => ref[data].classList.add('dark'));
-//   return array;
-// }
+  const object = Object.keys(ref);
 
-// function getArrayRemoveList(array) {
-//   array.forEach(data => ref[data].classList.remove('dark'));
-//   return array;
-// }
+  checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+      getArrayAddList(object);
+      localStorage.setItem('theme', 'dark');
+    } else {
+      getArrayRemoveList(object);
+      localStorage.setItem('theme', 'light');
+    }
+  });
+
+  function getArrayAddList(array) {
+    array.forEach(data => ref[data].classList.add('dark'));
+    return array;
+  }
+
+  function getArrayRemoveList(array) {
+    array.forEach(data => ref[data].classList.remove('dark'));
+    return array;
+  }
+}

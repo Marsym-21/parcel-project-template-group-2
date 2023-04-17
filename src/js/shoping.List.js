5f1f7a20cd182a7f107b9fe94dc4efc53636036b
roman10rb <roma10rb@gmail.com>
2023-04-17 09:01:00 +0300

    add some change in chopping list card

--- src/js/shoping.List.js
+++ src/js/shoping.List.js
@@ -2,7 +2,7 @@ import { getBookData } from './getBooksData';
 const logoPath = new URL('../images/icons.svg', import.meta.url);
 const shopingListEl = document.querySelector('.shopingList');
 
-const key = ['643282b2e85766588626a144', '643282b2e85766588626a144', '643282b2e85766588626a144', '643282b2e85766588626a144', '643282b2e85766588626a144', '643282b2e85766588626a144'];
+const key = ['643282b2e85766588626a144', '643282b2e85766588626a0f2', '643282b2e85766588626a0f2', '643282b3e85766588626a194', '643282b2e85766588626a144', '643282b2e85766588626a144'];
  
 stringKey = JSON.stringify(key);
 
@@ -31,47 +31,53 @@ function renderBook() {
             } 
           } 
         } 
-        return `<li class="card-item"> 
-          <div class="img-containerandAuthor"> 
-            <img class="book-photo" src="${book_image}" alt="#"> 
-            <p class="author">${author}</p> 
-          </div> 
-          <div class="books-title"> 
-            <p class="booktitle">${title}</p> 
-            <p class="bookCategory">${list_name}</p> 
-            <ul class="links">  
-              <li><a href="${links( 
+        return `
+<li class="shoppinglist_item">
+    <div class="shoppinglist_img-container">
+        <img class="shoppinglist_img" src="${book_image}" alt="" />
+        <p class="shoppinglist_authorM">${author}</p>
+    </div>
+    <div class="shoppinglist_info">
+        <h2 class="shoppinglist_title">${title}</h2>
+        <p class="shoppinglist_category">
+            ${list_name}
+        </p>
+    </div>
+    <ul class="shoppinglistlink_list">
+        <li>
+            <a href="${links( 
                 'Amazon' 
-              )}" target="_blank" rel="noopener noreferrer" class="amazon">  
-                 <svg fill="none" width="60" height="60">
-          <use href="${logoPath}#icon-amazon-ar21"></use>
-</svg>
-                </a></li>  
-              <li>  
-<a href="${links('Apple Books')}" target="_blank" rel="noopener noreferrer" class="amazon">  
-  <svg   width="30" height="30" fill="none">
-       <use href="${logoPath}#icon-apple-ibooks"></use> 
-       </svg>
-   </a>  
-              </li>  
-              <li>  
-<a href="${links('Bookshop')}" target="_blank" rel="noopener noreferrer" class="amazon">  
-    <svg   width="40" height="40">
-              <use href="${logoPath}#icon-14008711"></use>
-       </svg>
-   </a>  
-              </li>  
-            </ul>  
-          <div class="removee"> 
-           <svg  class="dumb" width="28" height="28">
-              <use href="${logoPath}#icon-dump-1"></use>
-       </svg>
-          </div> 
-        </div> 
-        <div class="description-text" 
-        <p class="description">${description}</p> 
-        </div> 
-      </li> 
+              )}" target="_blank" rel="noopener noreferrer">
+                <svg fill="none" width="55" height="60">
+                        <use href="${logoPath}#icon-amazon-ar21"></use>
+                    </svg>
+            </a>
+        </li>
+        <li>
+            <a href="${links('Apple Books')}" target="_blank" rel="noopener noreferrer">
+                <svg width="28" height="27" fill="none">
+                        <use href="${logoPath}#icon-apple-ibooks"></use>
+                    </svg>
+            </a>
+        </li>
+        <li>
+            <a href="${links('Bookshop')}" target="_blank" rel="noopener noreferrer">
+                <svg width="32" height="32">
+                        <use href="${logoPath}#icon-14008711"></use>
+                    </svg>
+            </a>
+        </li>
+    </ul>
+    <p class="shoppinglist_desc">
+        ${description}
+    </p>
+    <p class="shoppinglist_author">${author}</p>
+    <button class="shoppinglist_btn" type="button">
+        <svg class="shoppinglist_btn-icon">
+            <use href="${logoPath}#icon-dump-1"></use>
+        </svg>
+    </button>
+</li>
    `; 
       } 
     ); 
@@ -79,8 +85,12 @@ function renderBook() {
     shopingListEl.insertAdjacentHTML('beforeend', data); 
   }); 
 } 
-renderBook();
+  renderBook();
+getBook.getPromTopBooks().then(data => {
+      console.log(data)
+})
 })
 
 
-    
+
+    console.log('hello')
